4900e289634e909b536e0157bd69add7def21e14
Catarina <Catarina@DESKTOP-U7785QR>
2023-04-09 01:30:49 -0300

    Refactor PurchaseOrder

--- Products.Service/WorkFlow/PurchaseOrderService.cs
+++ Products.Service/WorkFlow/PurchaseOrderService.cs
@@ -9,25 +9,57 @@ namespace Products.Service.WorkFlow
     {
         private readonly IPurchaseOrderRepository _repository;
 
+        public decimal TotalPrice { get; set; } = 0;
+
+        public Queue<Product> ProductFifo { get; set; } = new Queue<Product>();
+
         public PurchaseOrderService(IPurchaseOrderRepository repository)
         {
             _repository = repository;
         }
-        public PurchaseOrder CreatePurchaseOrder(PurchaseOrderDTO poDTO)
+        //public PurchaseOrder CreatePurchaseOrder(PurchaseOrderDTO poDTO)
+        //{
+        //    var dateTime = DateTime.Now;
+
+        //    TotalPrice = CalculatePrice(poDTO.Products);
+        //    ProductFifo = CreateProductList(poDTO.Products);
+
+        //    PurchaseOrder po = new()
+        //    {
+        //        PaymentMethod = poDTO.PaymentMethod,
+        //        OrderNumber = $"BRSOB{dateTime.Year}{dateTime.Day}{dateTime.Month}",
+        //        TotalPrice = TotalPrice,
+        //    };
+
+        //    return po;
+        //}
+        private Queue<Product> CreateProductList(List<Product> products)
         {
-            var dateTime = DateTime.Now;
-
-            Queue<Product> productListFIFO = CreateProductToOrder(poDTO);
-            var totalPrice = CalculatePrice(poDTO.Products);
-
-            PurchaseOrder po = new()
+            foreach (var product in products)
             {
-                PaymentMethod = poDTO.PaymentMethod,
-                OrderNumber = $"BRSOB{dateTime.Year}{dateTime.Day}{dateTime.Month}",
-                TotalPrice = totalPrice
-            };
-
-            return po;
+                var item = _repository.QueryOrder($"SELECT * " +
+                                                    $"FROM [dbo].Products " +
+                                                    $"WHERE name = '{product.Name}' " +
+                                                    $"AND Quantity > 0 " +
+                                                    $"AND IsActive = 1" +
+                                                    $"UPDATE [dbo].Products " +
+                                                    $"SET Quantity = (SELECT Quantity FROM [dbo].Products " +
+                                                    $"WHERE id = {product.Id}) - {product.Quantity}").FirstOrDefault();
+                if (item != null)
+                {
+                    ProductFifo.Enqueue(item);
+                }
+            }
+            return ProductFifo;
+        }
+        private decimal CalculatePrice(List<Product> products)
+        {
+            foreach (var product in products)
+            {
+                decimal productPrice = product.Price * product.Quantity;
+                TotalPrice += productPrice;
+            }
+            return TotalPrice;
         }
         public IEnumerable<PurchaseOrder> GetAllOrders()
         {
@@ -37,46 +69,6 @@ namespace Products.Service.WorkFlow
         public PurchaseOrder GetPurchaseOrder(int id)
         {
             throw new NotImplementedException();
-        }
-        private Queue<Product> CreateProductToOrder(PurchaseOrderDTO poDTO)
-        {
-            var poDTOProducts = poDTO.Products;
-
-            Queue<Product> productsFIFO = new();
-
-            foreach (var product in poDTOProducts)
-            {
-                var item = _repository.QueryOrder($"SELECT * FROM [dbo].Products WHERE name = '{product.Name}' AND Quantity > 0 AND IsActive = 1").FirstOrDefault();
-                if (item != null)
-                {
-                    productsFIFO.Enqueue(item);
-                    UpdateDatabaseProducts(poDTOProducts);
-                }
-
-            }
-
-            return productsFIFO;
-        }
-        private void UpdateDatabaseProducts(List<Product> poList)
-        {
-            foreach (var product in poList)
-            {
-                _repository.QueryOrder($"UPDATE [dbo].Products SET Quantity = (SELECT Quantity FROM [dbo].Products WHERE id = {product.Id}) - {product.Quantity}");
-            }
-        }
-        private decimal CalculatePrice(List<Product> poList)
-        {
-            decimal totalPrice = 0;
-            foreach (var product in poList)
-            {
-                var priceQuery = _repository.QueryOrder($"SELECT SUM({product.Quantity} * {product.Price}) AS TotalPrice FROM [dbo].Products" +
-                                                        $"WHERE name = '{product.Name}' " +
-                                                        $"AND Quantity > 0 " +
-                                                        $"AND IsActive = 1").ToList();
-
-                totalPrice += priceQuery.Sum(x => x.Price);
-            }
-            return totalPrice;
         }
 
     }
